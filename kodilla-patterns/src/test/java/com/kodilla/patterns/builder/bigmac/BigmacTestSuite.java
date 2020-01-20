400141200029cbe216cd356df67004c2f407dceb
tomaszPietkun <tomasz.pietkun.99@gmail.com>
2020-01-20 01:36:45 +0100

    kodilla patterns 5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Onion")
+                .ingredient("Pickle")
+                .sauce("Honey-Mustard Sauce")
+                .ingredient("Lettuce")
+                .bun("Ciabatta Roll")
+                .burgers(2)
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(3, howManyIngredients);
+    }
+}
