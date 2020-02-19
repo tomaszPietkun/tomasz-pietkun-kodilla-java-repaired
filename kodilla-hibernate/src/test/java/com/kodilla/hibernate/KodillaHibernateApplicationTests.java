b4f18a0f573caa34dc41359d1241a586763674f1
tomaszPietkun <tomasz.pietkun.99@gmail.com>
2020-02-19 12:42:22 +0100

    hibernate 1.1

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class KodillaHibernateApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void contextLoad() {
+
     }
 }
