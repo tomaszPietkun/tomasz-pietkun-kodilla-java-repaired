e5ef257b615c51f8e495e905f2872fefe9830bf3
tomaszPietkun <tomasz.pietkun.99@gmail.com>
2020-05-27 21:16:04 +0200

    24.3

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -3,7 +3,9 @@ package com.kodilla.patterns2.decorator.pizza;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class PizzaOrderTestSuite {
     @Test
