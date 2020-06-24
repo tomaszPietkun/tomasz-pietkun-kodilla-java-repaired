7b18ff82d572cce8f108873e02e30bf16b2282d0
tomaszPietkun <tomasz.pietkun.99@gmail.com>
2020-06-24 13:25:35 +0200

    31.2

--- kodilla-testing-2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing-2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,20 +1,19 @@
-package com.kodilla.testing2.google;
+package com.kodilla.testing2.ebay;
 
 import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class GoogleTestingApp {
-
-    public static final String SEARCHFIELD = "gLFyf";
+public class EBayTestingApp {
+    public static final String SEARCHFIELD = "gh-ac";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
-        driver.get("https://www.google.com");
+        driver.get("https://wwww.ebay.com");
 
-        WebElement searchField = driver.findElement(By.className(SEARCHFIELD));
-        searchField.sendKeys("Kodilla");
+        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
+        searchField.sendKeys("Laptop");
         searchField.submit();
     }
 }
